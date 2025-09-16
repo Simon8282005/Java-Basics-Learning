b7a49a8aa425264cb665d7b1056f95081f63d36e
Simon8282005 <simonjava2019@gmail.com>
2025-09-16 13:47:39 +0800

    Commit

--- 4-7-2025/UI test/src/Main.java
+++ 4-7-2025/UI test/src/Main.java
@@ -2,7 +2,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.jar.JarEntry;
 
 public class Main extends JFrame {
     public Main() {
